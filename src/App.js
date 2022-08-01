a42a3c6c3f9750380bd230f15ee7a8f7896a2eb2
bvchnguyen <bang4509@colorado.edu>
2022-08-01 15:12:50 -0600

    Upload resume pdf and update app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Landing from "./components/Landing/Landing";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -5,6 +6,7 @@ function App(){
     return (
         <div className="App">
             <Navbar />
+            <Landing />
         </div>
     );   
 }
